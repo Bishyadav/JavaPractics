e5147a3602df994d6034c6679fdff2e9807b70d6
Bishyadav <129359916+Bishyadav@users.noreply.github.com>
2025-02-18 11:23:47 +0530

    TwoSumArray

--- src/bishesh/twoPointer/twoSum.java
+++ src/bishesh/twoPointer/twoSum.java
@@ -1,20 +1,17 @@
 package src.bishesh.twoPointer;
-
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Map;
 
 public class twoSum {
     public static void main(String[] args) {
         int[] arr={2,5,4,7,9,2};
-        int[] result=new int[2];
-        twoSum(arr,result);
-        for (int i= 0;i<result.length;i++){
-            System.out.println(result[i]+" ");
+        twoSum(arr);
+        for (int res:twoSum(arr)){
+            System.out.println(res+" ");
         }
     }
-    public static int[] twoSum(int[] arr,int[] result){
-        int target=9;
+    public static int[] twoSum(int[] arr){
+        int target=4;
+        int[] result=new int[2];
         HashMap<Integer,Integer> hm=new HashMap<>();
 
         for(int i=0;i<arr.length;i++){
