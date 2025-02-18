d8a1afef1010c644c54fb84d60e029f18cb29da6
Bishyadav <129359916+Bishyadav@users.noreply.github.com>
2025-02-18 17:15:03 +0530

    PrimeNo

--- src/bishesh/primeNo/PrintPrimeNo.java
+++ src/bishesh/primeNo/PrintPrimeNo.java
@@ -2,18 +2,36 @@ package src.bishesh.primeNo;
 
 public class PrintPrimeNo {
     public static void main(String[] args) {
-        int no=17;
+        int no=16;
         System.out.println(isPrime(no));
     }
     public static  boolean isPrime(int no){
         boolean flag=false;
         int count=0;
-        for(int i=1;i<=no;i++){
+        //first wat to find prime no
+        //for(int i=1;i<=no;i++){
+
+        //second way to find prime no
+//        for(int i=1;i<=no/2;i++){
+//        if (no%i==0){
+//            count++;
+//        }
+//    }
+//        if (count==1){
+//        flag=true;
+//    }
+//        else {
+//        flag=false;
+//    }
+//        return flag;
+
+        //third way to find prime no
+         for(int i=1;i<Math.sqrt(no);i++){
             if (no%i==0){
                 count++;
             }
         }
-        if (count==2){
+        if (count==1){
             flag=true;
         }
         else {
