f63111a63974e06807442d1ee72131c737af22d4
Bishyadav <129359916+Bishyadav@users.noreply.github.com>
2025-02-19 08:29:26 +0530

    AllPrimeNo

---
+++ src/bishesh/primeNo/FindNthPrimeNo.java
@@ -0,0 +1,30 @@
+package src.bishesh.primeNo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+public class FindNthPrimeNo {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter the nth no");
+        int userInput=s.nextInt();
+       int data=result(userInput);
+       System.out.println(data);
+    }
+    public static int result(int userInput){
+        List<Integer> list=new ArrayList<>();
+        for(int i=2;i<=4000;i++){
+            int count=0;
+            for(int j=1;j<=i;j++){
+               if (i%j==0){
+                  count++;
+               }
+            }
+            if (count==2){
+                list.add(i);
+            }
+        }
+       int finalResult=list.get(userInput-1);
+        return finalResult;
+    }
+}
