68b45c505579fb30d076e7b615c826b6c090f799
Bishyadav <129359916+Bishyadav@users.noreply.github.com>
2025-02-24 23:23:47 +0530

    ContainerWithMostWater

--- src/bishesh/twoPointer/ContainerWithMostWater.java
+++ src/bishesh/twoPointer/ContainerWithMostWater.java
@@ -2,17 +2,17 @@ package src.bishesh.twoPointer;
 
 public class ContainerWithMostWater {
     public static void main(String[] args) {
-        int[] arr={3,8,4,6,7,11,9};
+        int[] arr={2, 1, 8, 6, 4, 6, 5, 5};
        int maxValue=result(arr);
        System.out.print(maxValue);
     }
     public static int result(int[] arr){
         int i=0;
         int j=arr.length-1;
-        int index=j-i;
         int mostWater=0;
         int temp=0;
         while (i<j) {
+            int index=j-i;
             if (arr[i] < arr[j]) {
                 temp = arr[i];
                 i++;
@@ -24,7 +24,9 @@ public class ContainerWithMostWater {
                 i++;
             }
             temp = temp * index;
-            mostWater = mostWater + temp;
+            if (mostWater<temp){
+                mostWater=temp;
+            }
         }
         return mostWater;
         }
