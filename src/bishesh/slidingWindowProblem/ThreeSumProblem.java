a96e0240f34a68b8e0d53324c0a3af22da3565e9
Bishyadav <129359916+Bishyadav@users.noreply.github.com>
2025-02-25 00:05:37 +0530

    WindowProblem

--- src/bishesh/slidingWindowProblem/ThreeSumProblem.java
+++ src/bishesh/slidingWindowProblem/ThreeSumProblem.java
@@ -9,22 +9,46 @@ public class ThreeSumProblem {
         }
     }
     public static  int[] findIndex(int[] a){
-        int k=3;
+//       int k=3;
+//        int target=7;
+//        int sum=0;
+//       int[] result=new int[2];
+//       for(int i=0;i<k;i++){
+//         sum=sum+a[i];
+//       }
+//
+//       for(int i=k;i<a.length;i++){
+//           sum=sum+a[i]-a[i-k];
+//           if (sum==target){
+//               result[0]=i-2;
+//               result[1]=i;
+//               break;
+//           }
+//       }
+        //OR 2nd Way
         int target=7;
-        int sum=0;
-       int[] result=new int[2];
-       for(int i=0;i<k;i++){
-         sum=sum+a[i];
-       }
-
-       for(int i=k;i<a.length;i++){
-           sum=sum+a[i]-a[i-k];
-           if (sum==target){
-               result[0]=i-2;
-               result[1]=i;
-               break;
-           }
-       }
-       return result;
+        int[] arr=new int[2];
+        for(int i=0;i<a.length-2;i++){
+            int j=i+1;
+            int sum=0;
+            int k=1;
+            while (k<=2){
+                if (k<=1){
+                 sum=sum+(a[i]+a[j]);
+                 j++;
+                 k++;
+                }
+                else{
+                    sum=sum+a[j];
+                    k++;
+                }
+            }
+            if (sum==target){
+              arr[0]=i;
+              arr[1]=j;
+              break;
+            }
+        }
+       return arr;
     }
 }
